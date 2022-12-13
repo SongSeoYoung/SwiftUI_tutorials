693850c505d62c6d635dab26c54826ea047df994
Seori(송서영) seori.song@bunjang.co.kr <seori.song@bunjang.co.kr>
2022-12-14 01:15:31 +0900

    Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Seori on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
