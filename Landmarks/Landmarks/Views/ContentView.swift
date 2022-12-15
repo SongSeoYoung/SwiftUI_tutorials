cfcdc3e225929074bdfbe4bf8988aaea72f2f31e
Seoyoung Song <bgd216@naver.com>
2022-12-15 23:30:44 +0900

    building-lists-and-navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Seori on 2022/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13 Pro Max")
+    }
+}
--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -1,51 +1,53 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  Landmarks
 //
-//  Created by Seori on 2022/12/13.
+//  Created by Seori on 2022/12/15.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView().frame(height: 300)
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
             
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+                
                 HStack {
                     Text("Joshua Tree National Park")
                     Spacer()
                     Text("California")
-                        
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
                 
                 Divider()
                 
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
-                
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
             }
             .padding()
-            
-            Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail(landmark: landmarks[1])
             .previewDevice("iPhone 13 Pro Max")
     }
 }
